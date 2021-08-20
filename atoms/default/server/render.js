a3948ce2bee5a506e3818fd157b3b06c6c2a74c6
pradasa <ana.pradas@gmail.com>
2021-08-20 10:48:18 +0100

    added Source space.scss, linked content to spreadsheet

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,15 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+import mainTemplate from "./atoms/default/server/templates/main.html!text"
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainHTML;
-} 
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1Ou7iiqispIBnZWNrT1WyhYxirUbeyrqV_sykX4QG7X0.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
+}
